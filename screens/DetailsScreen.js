31af3335b6b10218c4a6cf510991155ed4821df2
irem.erim <iremerim34@gmail.com>
2024-06-14 13:34:36 +0300

    refresh added for details screen

--- screens/DetailsScreen.js
+++ screens/DetailsScreen.js
@@ -1,16 +1,35 @@
 // screens/DetailsScreen.js
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, ScrollView, RefreshControl } from 'react-native';
 
 const DetailsScreen = ({ navigation }) => {
+
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go back"
-        onPress={() => navigation.goBack()}
-      />
-    </View>
+    <ScrollView 
+      contentContainerStyle={styles.scrollView}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
+    >
+      <View style={styles.container}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go back"
+          onPress={() => navigation.goBack()}
+        />
+      </View>
+    </ScrollView>
+    
   );
 };
 
@@ -20,6 +39,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  scrollView: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
 
 export default DetailsScreen;
